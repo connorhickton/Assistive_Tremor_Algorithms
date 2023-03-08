76e2092bfbc2f73a7f787e57e46f5cf5b8457527
Connor Hickton <connorhickton@gmail.com>
2023-03-08 09:50:11 -0800

    sharedfunctions.py modified after renaming

--- sharedfunctions.py
+++ sharedfunctions.py
@@ -73,6 +73,53 @@ def getDirection(coords, oldElement):
 
     return direction
 
+# Movement Variability evaluation.
+# Takes the start and end target (1 and 2 respectively), the size of the targets (always the same), 
+# and the mouse's path between them (list of tuples, first element containing coords in another tuple, second element containing time).
+# From MacKenzie et al, "Accuracy Measures for Evaluating Computer Pointing Devices" (2001)
+def eval1(tgt1, tgt2, radius, path):
+    
+    
+    x1, y1 = tgt1
+    x2, y2 = tgt2
+    dx, dy = x2-x1, y2-y1
+
+    yDistFromAxis = []
+
+    for i in range(len(path)):
+        
+        # get closest distance from task axis
+        # https://stackoverflow.com/questions/47177493/python-point-on-a-line-closest-to-third-point
+
+        x3, y3 = path[i][0]
+        
+        det = dx * dx + dy * dy
+        a = (dy*(y3-y1)+dx*(x3-x1))/det
+        nearestOnLine = x1+a*dx, y1+a*dy
+
+        yDistFromAxis.insert(0,math.hypot(nearestOnLine[0]-x3, nearestOnLine[1]-y3))
+
+    meanDist = (sum(yDistFromAxis) / len(yDistFromAxis))
+
+    summation = 0
+    for j in range(len(yDistFromAxis)):
+        summation += ((yDistFromAxis[j]-meanDist) ** 2)
+    
+    if (len(yDistFromAxis) - 1) == 0:
+        return None
+    
+    equation = summation / (len(yDistFromAxis) - 1)
+
+    equation = math.sqrt(equation)
+
+    return equation
+
+# Test of eval1
+import math
+tstPath = [[[1,10],100],[[3,8],102], [[6,6],104], [[8,2],106]]
+print(eval1([0,10], [10,0], 5, tstPath))
+
+
 
 # Banihashem et al's specific breakpoint logic.
 # If the direction has changed from X seconds ago (X = 0.5 in the paper) and the mouse has moved more than BREAKVAL pixels, a breakpoint is registered.
@@ -98,7 +145,9 @@ def getAngle(a, b, c):
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
     return ang + 360 if ang < 0 else ang
  
-print(getAngle((98, 50), (81, 66), (112, 71)))
+# print(getAngle((98, 50), (81, 66), (112, 71)))
+
+
 
 
 # my own breakpoint algorithm. Looks at three coordinates: The current mouse position, the coordinates X seconds ago, and the coordinates X seconds again before that.
